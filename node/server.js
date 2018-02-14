66438e83eca49d8e795432ae0bba38093f4f0411
Kyle Desktop <kylespresser@gmail.com>
2018-02-14 14:51:57 -0500

    165

--- node/server.js
+++ node/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('<h1>Hello there</h1>');
-  next();
-})
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('testtest');
+  res.send('getting root');
+});
+
+app.get('/profile', (req, res) => {
+  res.send('getting profile');
+});
+
+app.post('/profile', (req, res) => {
+  const user = req.body;
+  res.send(user);
 });
 
 
