af2e4eaed37a4a9f7b478e3f29cdb9a7afc20147
Kyle Desktop <kylespresser@gmail.com>
2018-02-15 10:11:52 -0500

    170

--- final-project/smart-brain-api/server.js
+++ final-project/smart-brain-api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const bcrypt = require('bcrypt-nodejs')
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,7 +12,6 @@ const database = {
       id: '123',
       name: 'John',
       email: 'john@gmail.com',
-      password: 'cookies',
       entries: 0,
       joined: new Date()
     },
@@ -20,9 +19,15 @@ const database = {
       id: '124',
       name: 'Sally',
       email: 'sally@gmail.com',
-      password: 'bananas',
       entries: 0,
       joined: new Date()
+    }
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@gmail.com'
     }
   ]
 }
@@ -41,6 +46,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
+  bcrypt.hash(password, null, null, function(err, hash) {
+    console.log(hash);
+  });
   database.users.push({
     id: '125',
     name: name,
@@ -80,6 +88,21 @@ app.put('/image', (req, res) => {
     res.status(404).json('not found');
   }
 })
+
+
+
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//   //Store hash in your password DB.
+// });
+//
+// //Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//   //res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//   //res = false
+// });
+
 
 app.listen(3000, () => {
   console.log('working for the man every night and day.....');
