12995c3c37615acbfc2b1c657f2aaa58b50a192b
Kyle Desktop <kylespresser@gmail.com>
2018-02-16 06:50:22 -0500

    192

--- final-project/smart-brain-api/server.js
+++ final-project/smart-brain-api/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const knex = require('knex');
 
 
-const postgres = knex({
+const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
@@ -15,7 +15,9 @@ const postgres = knex({
   }
 });
 
-console.log(postgres.select('*').from('users'));
+db.select('*').from('users').then(data => {
+  console.log(data);
+});
 
 
 const app = express();
@@ -57,17 +59,17 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  // bcrypt.hash(password, null, null, function(err, hash) {
-  //   console.log(hash);
-  // });
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users[database.users.length-1])
+  db('users')
+    .returning('*')
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date()
+    })
+    .then(user => {
+      res.json(user[0])
+    })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
